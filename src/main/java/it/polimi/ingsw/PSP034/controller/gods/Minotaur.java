1235aaa85d9c1785292c92df7017e5d13fc6b310
Veronica-gg <61884217+Veronica-gg@users.noreply.github.com>
2020-04-09 23:18:58 +0200

    Added Zeus, fixed minor errors in Ephaestus and Minotaur

--- src/main/java/it/polimi/ingsw/PSP034/controller/gods/Minotaur.java
+++ src/main/java/it/polimi/ingsw/PSP034/controller/gods/Minotaur.java
@@ -16,6 +16,12 @@ public class Minotaur extends GodsRules {
     }
 
     @Override
+    protected Player getPlayer() {
+        return super.getPlayer();
+    }
+
+
+    @Override
     public TurnPhase nextState(TurnPhase currentPhase){
         return super.nextState(currentPhase);
     }
@@ -96,5 +102,10 @@ public class Minotaur extends GodsRules {
         }
         else
             return false;
+    }
+
+    @Override
+    public boolean checkWin(Worker worker){
+        return super.checkWin(worker);
     }
 }
---
+++ src/main/java/it/polimi/ingsw/PSP034/controller/gods/Zeus.java
@@ -0,0 +1,61 @@
+package it.polimi.ingsw.PSP034.controller.gods;
+
+import it.polimi.ingsw.PSP034.constants.*;
+import it.polimi.ingsw.PSP034.controller.GodsRules;
+import it.polimi.ingsw.PSP034.controller.IRules;
+import it.polimi.ingsw.PSP034.model.*;
+
+public class Zeus extends GodsRules {
+    private boolean builtUnderMe = false;
+
+    public Zeus(IRules decoratedRules, Player player){
+        super(decoratedRules, player);
+    }
+
+    @Override
+    protected Player getPlayer() {
+        return super.getPlayer();
+    }
+
+    @Override
+    public TurnPhase nextState(TurnPhase currentPhase) {
+        return super.nextState(currentPhase);
+    }
+
+    @Override
+    public Boolean executeState(TurnPhase currentPhase, Worker worker, Tile tile, Boolean choice) {
+        return super.executeState(currentPhase, worker, tile, choice);
+    }
+
+    @Override
+    public boolean validMove(Worker worker, Tile destinationTile) {
+        return super.validMove(worker, destinationTile);
+    }
+
+    @Override
+    public boolean validBuild(Worker worker, Tile buildingTile) {
+        if(getPlayer().isOwner(worker)){
+            if (!buildingTile.equals(worker.getMyTile())){
+                if(!super.getDefaultRules().validBuild(worker, buildingTile)){
+                    return false;
+                }
+            }else if (worker.getMyTile().getBuilding() == Constant.LEVEL_THREE){
+                return false;
+            }
+            else
+                builtUnderMe = true;
+        }
+        return validBuildRecursive(worker, buildingTile);
+    }
+
+    @Override
+    public boolean checkWin(Worker worker){
+        if(getPlayer().isOwner(worker)) {
+            if (builtUnderMe)
+                return false;
+            else if (!(getDefaultRules().checkWin(worker)))
+                return false;
+        }
+        return super.checkWinRecursive(worker);
+    }
+}
