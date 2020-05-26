26673db953d6947a0d69dfabb1bdb3e577cfdf0a
Lorenzo Guerrieri <lorenzo.guerrieri@mail.polimi.i>
2020-05-26 15:40:40 +0200

    Fixed Table.

--- src/main/java/it/polimi/ingsw/PSP034/view/CLI/printables/WinnerDrawing.java
+++ src/main/java/it/polimi/ingsw/PSP034/view/CLI/printables/WinnerDrawing.java
@@ -5,14 +5,14 @@ import it.polimi.ingsw.PSP034.constants.Colors;
 import it.polimi.ingsw.PSP034.constants.PlayerColor;
 
 public class WinnerDrawing extends PrintableObject{
-    public WinnerDrawing(PlayerColor color){
+    public WinnerDrawing(){
         super();
         String buildColor = Colors.BUILDING_BG.get();
-        String playerColor = color.getFG_color();
+        //String playerColor = color.getFG_color();
         super.setObjectSize(9);
-        super.setObjectLine(0, "   " + playerColor + "\\o/   " + ANSI.reset);
-        super.setObjectLine(1, "    " + playerColor + "█    " + ANSI.reset);
-        super.setObjectLine(2, "   " + playerColor + "/ \\   " + ANSI.reset);
+        super.setObjectLine(0, "   " + /*+ playerColor +*/ "\\o/   " + ANSI.reset);
+        super.setObjectLine(1, "    " + /*+ playerColor +*/ "█    " + ANSI.reset);
+        super.setObjectLine(2, "   " + /*+ playerColor +*/ "/ \\   " + ANSI.reset);
         super.setObjectLine(3, "  " + buildColor + "█████  " + ANSI.reset);
         super.setObjectLine(4, "  " + buildColor + "█████  " + ANSI.reset);
         super.setObjectLine(5, " " + buildColor + "███████ " + ANSI.reset);
